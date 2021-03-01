972a500b5ad2b0563e85ecc1c4f593a14fe31c7f
sdennett55@gmail.com <sdennett55@gmail.com>
2021-03-01 13:09:56 -0500

    white space change

--- utilities.js
+++ utilities.js
@@ -9,7 +9,7 @@ function injectContent(filepath, { header, body }) {
 
 function formatDate(timestamp) {
 
-	// Create a date object from the timestamp
+	// Create a date object from the timestamp.
 	var date = new Date(timestamp);
 
 	// Create a list of names for the months
